2248b6ef7a891619f0a98a11c799f1b08a1ad18a
Mayank Sharma <205119053@nitt.edu>
2021-04-22 19:02:56 +0530

    Refactored test_blog.py

--- blog/tests/unit/test_blog.py
+++ blog/tests/unit/test_blog.py
@@ -3,20 +3,18 @@ from blog import Blog
 
 
 class BlogTest(TestCase):
-    def test_create_blog(self):
-        b = Blog('Blog', 'Mayank')
+    def setUp(self):
+        self.b = Blog('Blog', 'Mayank')
 
-        self.assertEqual('Blog', b.title)
-        self.assertEqual('Mayank', b.author)
-        self.assertListEqual([], b.posts)
+    def test_create_blog(self):
+        self.assertEqual('Blog', self.b.title)
+        self.assertEqual('Mayank', self.b.author)
+        self.assertListEqual([], self.b.posts)
 
     def test_repr(self):
-        b = Blog('Blog', 'Mayank')
-
-        self.assertEqual(b.__repr__(), 'Blog by Mayank (0 posts)')
+        self.assertEqual(self.b.__repr__(), 'Blog by Mayank (0 posts)')
 
     def test_repr_multiple_posts(self):
-        b = Blog('Blog', 'Mayank')
-        b.posts = ['Test Post']
+        self.b.posts = ['Test Post']
 
-        self.assertEqual(b.__repr__(), 'Blog by Mayank (1 posts)')
+        self.assertEqual(self.b.__repr__(), 'Blog by Mayank (1 posts)')
