52accb35649b9053cf56cde4ff7c9d10df0cc6a1
Mayank Sharma <205119053@nitt.edu>
2021-04-22 18:50:10 +0530

    Added more menu system tests

--- blog/tests/system/test_app.py
+++ blog/tests/system/test_app.py
@@ -10,24 +10,45 @@ class AppTest(TestCase):
         blog = Blog('Test', 'Test Author')
         app.blogs = {'Test': blog}
 
+    def test_menu_print_blogs(self):
+        with patch('builtins.print') as mocked_print:
+            with patch('builtins.input', return_value='q'):
+                app.menu()
+                mocked_print.assert_called_with('- Test by Test Author (0 posts)')
+
+    def test_menu_prints_prompt(self):
+        with patch('builtins.input', return_value='q') as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+
     # todo - create test for various combinations of menu options, but do it
+
     def test_menu_calls_ask_create_blog(self):
         with patch('builtins.input') as mocked_input:
             mocked_input.side_effect = ('c', 'Test Blog', 'Mayank', 'q')
             app.menu()
             self.assertIsNotNone(app.blogs['Test Blog'])
 
-    # after adding Continuouos Integration
     def test_menu_calls_print_blogs(self):
-        with patch('app.print_blogs') as mocked_print_blogs:
-            with patch('builtins.input', return_value='q'):
+        with patch('builtins.input') as mocked_input:
+            with patch('app.print_blogs') as mocked_print_blogs:
+                mocked_input.side_effect = ('l', 'q')
                 app.menu()
                 mocked_print_blogs.assert_called()
 
-    def test_menu_prints_prompt(self):
-        with patch('builtins.input', return_value='q') as mocked_input:
-            app.menu()
-            mocked_input.assert_called_with(app.MENU_PROMPT)
+    def test_menu_calls_ask_read_blog(self):
+        with patch('builtins.input') as mocked_input:
+            with patch('app.ask_read_blog') as mocked_ask_read_blog:
+                mocked_input.side_effect = ('r', 'q')
+                app.menu()
+                mocked_ask_read_blog.assert_called()
+
+    def test_menu_calls_ask_create_post(self):
+        with patch('builtins.input') as mocked_input:
+            with patch('app.ask_create_post') as mocked_ask_create_post:
+                mocked_input.side_effect = ('p', 'q')
+                app.menu()
+                mocked_ask_create_post.assert_called()
 
     def test_print_blogs(self):
         with patch('builtins.print') as mocked_print:
